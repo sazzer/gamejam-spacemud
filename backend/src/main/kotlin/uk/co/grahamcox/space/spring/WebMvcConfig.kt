52bd14999465090419b41d8cb9d6c7e27176f289
Graham Cox <graham@grahamcox.co.uk>
2018-04-10 19:24:06 +0100

    Enabled Cors

--- backend/src/main/kotlin/uk/co/grahamcox/space/spring/WebMvcConfig.kt
+++ backend/src/main/kotlin/uk/co/grahamcox/space/spring/WebMvcConfig.kt
@@ -1,6 +1,7 @@
 package uk.co.grahamcox.space.spring
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -10,5 +11,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
  */
 @Configuration
 class WebMvcConfig : WebMvcConfigurer {
-
+    /**
+     * Allows cross-origin calls to any API handler
+     */
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/api/**");
+    }
 }
