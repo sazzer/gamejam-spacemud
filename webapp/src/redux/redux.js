4ed42de499c2099860fe3012e66eaf06a09cbe86
Graham Cox <graham@grahamcox.co.uk>
2018-04-19 12:39:06 +0100

    Redux support for creating a new user

--- webapp/src/redux/redux.js
+++ webapp/src/redux/redux.js
@@ -1,6 +1,8 @@
 import {createStore} from 'redux-box';
 import {userExistsModule} from '../authentication'
+import {createUserModule} from '../users'
 
 export default createStore([
-    userExistsModule
+    userExistsModule,
+    createUserModule
 ]);
